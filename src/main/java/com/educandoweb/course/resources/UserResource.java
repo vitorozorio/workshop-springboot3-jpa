4d3775b783dc62c1938030ebbafd0f84f1cbfa89
vitor ozorio <vitormanoelbasilio357@gmail.com>
2024-07-30 22:44:23 -0300

    organizando o codigo

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import java.net.URI;
 import java.util.List;
 
